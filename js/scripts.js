0caea36b1d0da7f17261a476dab2560228225a39
Chakri Reddy <chakrib2k@gmail.com>
2015-11-27 16:06:13 +0530

    All Panels upto 6th Created and transitions updated
    Transitions for all panels is set and 6th panel at end is also created

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,22 @@
        $("#descri").fadeIn(1000);
        $("#social").fadeIn(1000);
 
-
+//Srolling to division functions.........................................
        $("#logo").click(function() {
            $('html, body').animate({
                scrollTop: $("#third").offset().top
            }, 3000);
        });
 
+       $("#descri").click(function() {
+           $('html, body').animate({
+               scrollTop: $("#fourth").offset().top
+           }, 3000);
+       });
 
+
+
+//Srolling to division functions.........................................
        var n = $("#main div").length;
        var g = $("#second").length;
        var f = $("#third").length;
@@ -46,6 +54,21 @@
                    $("#fifth").show();
                }
 
+           }
+       });
+
+
+       $(window).scroll(function() {
+           if ($('#fourth').visible(true) && $('#fifth').visible(true) && $("#third").visible(true) == false) {
+
+               $("#sixth").show().slideDown(4000);
+
+               console.log("normal is going on");
+
+           } else {
+             // $("#sixth").slideUp(4000);
+               $("#sixth").hide().css("display", "none");
+                console.log("else is going on");
            }
        });
 
